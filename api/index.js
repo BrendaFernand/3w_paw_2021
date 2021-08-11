c9af0eb984331b3b3afefcf0b9d5fb15822a5aa0
BrendaFernand <brendafernandas324@gmail.com>
2021-08-11 15:02:38 -0300

    Aula dia 11/08

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const servidor = express()
+
+servidor.get("/", (req, res, next) => {
+     return res.send({
+     "mensagem": "Bem-vindo ao servidor Node.js"
+})
+})
+servidor.listen(3000, () => {
+     console.log("Servidor funcionando!")
+})
